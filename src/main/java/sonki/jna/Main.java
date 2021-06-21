8c2b0efca6619d0ba681ccf407f71bb197ab4002
sonkiWSL <sonke.a.wohler@gmail.com>
2021-06-21 16:20:09 +0300

    runs on both windows and linux

--- src/main/java/sonki/jna/Main.java
+++ src/main/java/sonki/jna/Main.java
@@ -2,11 +2,12 @@ package sonki.jna;
 
 import com.sun.jna.Native;
 import com.sun.jna.Library;
+import com.sun.jna.Platform;
 
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World of C calls");
-    CInterface lib = Native.load("c", CInterface.class);
+    CInterface lib = Native.load(Platform.isWindows() ? "msvcrt" : "c", CInterface.class);
     double result = lib.cosh(0);
     System.out.println("hyper cosine of 0 is " + result);
   }
